4f53bf9759a44e866a252f04fa088735df91c4e2
gitForDavidL <davidtach26@gmail.com>
2023-09-10 10:57:23 -0500

    added imgs at game mode

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
 
 interface GameMode {
   name: string;
+  id: number;
+}
+
+interface imagesModes{
+  name: string;
+  url: string;
   id: number;
 }
 
@@ -13,12 +19,14 @@ interface GameMode {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent  {
+export class HomeComponent {
 
   selectedValue: number;
-
-
+  images: imagesModes[];
+  imageSelected: string | undefined;
   gameModes: GameMode[]
+  showImage: boolean;
+  @ViewChild('logo') logo!: ElementRef;
 
   constructor(private toastr: ToastrService) {
 
@@ -27,22 +35,39 @@ export class HomeComponent  {
       { name: 'Completo', id: 1 },
       { name: 'Solo esquinas', id: 2 },
       { name: 'Solo esquinas y centro', id: 3 },
-      { name: 'L', id: 4 },
-      { name: 'T', id: 5 },
-      { name: 'B', id: 6 },
-      { name: 'X', id: 7 },
-      { name: 'Cruz', id: 8 },
-      { name: 'Cuadrado', id: 9 },
-      { name: 'U', id: 10 },
+      { name: 'Solo letra B', id: 4 },
+      { name: 'Solo letra I', id: 5 },
+      { name: 'Solo letra N', id: 6 },
+      { name: 'Solo letra G', id: 7 },
+      { name: 'Solo letra O', id: 8 },
     ]
 
+    this.images = [
+      {name: 'logo', url: 'https://i.ibb.co/7S85XhZ/inter-Logo.jpg', id: 0},
+      { name: 'Completo', url: 'https://i.ibb.co/Z6HPLb5/completo-removebg-preview.png', id: 1 },
+      { name: 'Solo esquinas', url: 'https://i.ibb.co/MZs7wvz/Esquinas-removebg-preview.png', id: 2 },
+      {name: 'Solo esquinas y centro', url: 'https://i.ibb.co/0GcBxpS/Esquinas-y-centro-removebg-preview.png', id: 3},
+      { name: 'Solo letra B', url: 'https://i.ibb.co/Z2sxc3r/B-removebg-preview.png', id: 4 },
+      { name: 'Solo letra I', url: 'https://i.ibb.co/ZcMGQxx/I-removebg-preview.png', id: 5 },
+      { name: 'Solo letra N', url: 'https://i.ibb.co/RBfN3db/N-removebg-preview.png', id: 6 },
+      { name: 'Solo letra G', url: 'https://i.ibb.co/gWSp2wC/G-removebg-preview.png', id: 7 },
+      { name: 'Solo letra O', url: 'https://i.ibb.co/R0RKZDd/O-removebg-preview.png', id: 8 },
+    ]
     this.selectedValue = this.gameModes[0].id;
+    this.imageSelected = "";
+    this.showImage = true;
+    this.changeOption(0);
   }
 
 
   changeOption(valor: number) {
-    console.log(valor);
     this.selectedValue = valor;
+    if (valor == 0 ){
+     this.showImage = true;
+     return;
+    }
+    this.imageSelected = this.images.find(x => x.id == valor)?.url;
+    this.showImage = false;
   }
 
   selectMode() {
@@ -50,8 +75,9 @@ export class HomeComponent  {
       this.toastr.warning('Selecciona un modo de juego', 'Alerta');
       return;
     }
-
     this.toastr.success('Modo de juego seleccionado', 'Correcto');
+    //cargar la imagen del modo de juego
+    localStorage.setItem('img', this.imageSelected!);
     window.location.href = '/bingo?mode=' + this.selectedValue;
 
   }
